46369b6670b35a8b68277d761605330d363fc598
Darien <dsis335@icloud.com>
2021-08-12 23:40:58 -0500

    Initial commit

---
+++ InvoiceUI/InvoiceUI/InvoiceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceUIApp.swift
+//  InvoiceUI
+//
+//  Created by Darien Sandifer on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
